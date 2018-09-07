10270fc8907416f094b51bc002bbfbb9e5a60604
isaacdu84 <isaacdu84@gmail.com>
2018-09-07 00:50:39 -0400

    fixed bug again...

--- app.py
+++ app.py
@@ -8,8 +8,29 @@ from bokeh.embed import components
 
 app = Flask(__name__)
 
-feature_names = ['AAPL','GOOG','INTC','KO','MSFT','T','HPQ','WMT']
 # plotting the data
+def create_figure(current_feature_name):
+    #Grab data from quandl
+    url_dataset = 'https://www.quandl.com/api/v3/datasets/WIKI/'
+    url_options = '/data.json?start_date=2018-01-01&end_date=2018-01-31&order=asc&column_index=4&api_key=VmrejGLj7UAexzD_wssd'  # only retrieve Jan 2018
+    api_url = url_dataset + current_feature_name + url_options
+    session = requests.Session()
+    raw_data = session.get(api_url)
+    data = raw_data.json()['dataset_data']
+    df = pd.DataFrame(data['data'], columns=['Date', 'Closing'])
+    
+    crosswalk = pd.read_csv("https://s3.amazonaws.com/quandl-static-content/Ticker+CSV%27s/secwiki_tickers.csv")
+    crosswalk_dict = dict(zip(crosswalk['Ticker'],crosswalk['Name']))
+
+    #Create a line plot
+    df['Date'] = pd.to_datetime(df['Date'])
+    source = ColumnDataSource(df)
+    p = figure(title='Quandl WIKI Closing Stock Prices - Jan 2018', plot_width=500, plot_height=500, x_axis_type='datetime')
+    p.line('Date', 'Closing', source=source, legend=crosswalk_dict[current_feature_name], line_width=2)
+    p.xaxis.axis_label = 'Date'
+    p.yaxis.axis_label = 'Price'
+    return p
+'''
 def create_figure(current_feature_name):
 	#Grab data from quandl
     url_dataset = 'https://www.quandl.com/api/v3/datasets/WIKI/'
@@ -19,7 +40,9 @@ def create_figure(current_feature_name):
     raw_data = session.get(api_url)
     data = raw_data.json()['dataset_data']
     df = pd.DataFrame(data['data'], columns=['Date', 'Closing'])
-
+	crosswalk = pd.read_csv()
+	df = df
+	
     #Create a line plot
     df['Date'] = pd.to_datetime(df['Date'])
     source = ColumnDataSource(df)
@@ -29,7 +52,7 @@ def create_figure(current_feature_name):
     p.xaxis.axis_label = 'Date'
     p.yaxis.axis_label = 'Price'
     return p
-
+'''
 
 @app.route('/')
 def index():
